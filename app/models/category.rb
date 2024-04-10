cb3b51ca5c535efa8c9995a0e52c226bab259321
SumailKaur777 <156945133+SumailKaur777@users.noreply.github.com>
2024-04-10 11:56:31 -0500

    Integrated database migrations

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,7 @@
 class Category < ApplicationRecord
     has_many :products
   
- validates :name, presence: true, uniqueness: true
+ 
+    validates :name, presence: true, uniqueness: true
   end
   
